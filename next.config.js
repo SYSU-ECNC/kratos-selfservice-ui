d866b5042cde464eb8e851620ea848a5e3a52971
Robin Wong <chaowang0313@gmail.com>
2022-03-12 19:14:04 +0000

    feat: upgrade NextJS and drop .babelrc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
   experimental: {
     outputStandalone: true,
   },
